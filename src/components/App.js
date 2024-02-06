1e0f362574113e455100233153e4d34ae5513510
Genevieve Strittmatter <eastrittmatter@gmail.com>
2024-02-06 09:05:17 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import { useEffect, useState } from "react";
+
+function App() {
+
+    const [ image, setImage ] = useState();
+    const [ isLoaded, setIsLoaded ] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(dog => {setImage(dog); setIsLoaded(true);});
+    }, []);
+
+    if (!isLoaded) {return <p>Loading...</p>;}
+    console.log(image.message)
+    return <img src={image.message} alt="A Random Dog" />;
+}
+
+export default App;
